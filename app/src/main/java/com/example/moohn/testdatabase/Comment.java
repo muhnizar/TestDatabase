1e094908755c3e0d187870b83f8ebc126e4b5eb5
muhnizar <mooh.nizar@yahoo.com>
2016-02-17 05:38:58 +0700

    +add model comment

---
+++ app/src/main/java/com/example/moohn/testdatabase/Comment.java
@@ -0,0 +1,22 @@
+package com.example.moohn.testdatabase;
+
+public class Comment {
+    private long id;
+    private String comment;
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
