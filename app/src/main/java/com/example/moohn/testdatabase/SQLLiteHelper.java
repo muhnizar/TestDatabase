9d34c1c69b2803ba13a7bcc70fa5ec970313d00c
muhnizar <mooh.nizar@yahoo.com>
2016-02-17 05:35:52 +0700

    +add sqlLiteHelper

--- app/src/main/java/com/example/moohn/testdatabase/SQLLiteHelper.java
+++ app/src/main/java/com/example/moohn/testdatabase/SQLLiteHelper.java
@@ -3,6 +3,7 @@ package com.example.moohn.testdatabase;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 public class SQLLiteHelper extends SQLiteOpenHelper {
 
@@ -12,7 +13,8 @@ public class SQLLiteHelper extends SQLiteOpenHelper {
     public static final String DATABASE_NAME = "comments.db";
     public static final int DATABASE_VERSION = 1;
 
-
+    public static final String DATABASE_CREATE = "create table " + TABLE_COMMENTS + " ( " + COLUMN_ID +" integer primary key autoincrement, " +
+            COLUMN_COMMENT + " text not null); ";
 
     public SQLLiteHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -20,11 +22,13 @@ public class SQLLiteHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        db.execSQL(DATABASE_CREATE);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        Log.w(SQLLiteHelper.class.getName(), "Upgrading database from version" +oldVersion +" to version "+newVersion);
+        db.execSQL("DROP TABLE IF EXIST " +TABLE_COMMENTS);
+        onCreate(db);
     }
 }
